8f18293edc4468328f120e7b99d11ec03cf04e55
Duong Duc Son <sndc0969755036@gmail.com>
2020-07-08 17:32:59 +0700

    mmm

---
+++ abc.js
@@ -0,0 +1,15 @@
+console.log(2+1)
+i = 2;
+console.log(i)
+for(i = 1; i < 10; i++)
+{
+    console.log(i);
+}
+ name = 'viet nam muon nam';
+ console.log('vi tru thu 2: '+name.charAt(2));
+// console.log('name co: ' + name.length);
+console.log(`name co ${name.length} ky tu`);
+
+var str = "Hello world, welcome to the universe.";
+var n = str.endsWith("universe.");
+console.log(n);
---
+++ bt.js
@@ -0,0 +1,19 @@
+function bp(x){
+    console.log(x*x)
+    }
+bp(6);
+
+ function cv(x,y){
+     console.log(2*(x+y));
+        }
+cv(5,6);
+
+function cc(n){
+    for(i = 0; i< 10; i++){
+        kq = n*i;
+        console.log(` ${n} * ${i} = ${kq} `);
+    }
+               }
+cc(3);
+
+
